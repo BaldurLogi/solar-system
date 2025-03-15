95482b90b6145e1565e99ef529889757cd72933e
Baldur Logi <134936299+BaldurLogi@users.noreply.github.com>
2025-03-15 15:54:41 +0000

    try

--- app.js
+++ app.js
@@ -12,8 +12,10 @@ app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
 mongoose.connect(
-    process.env.MONGO_URI,
-  )
+    process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+  })
     .then(() => console.log("MongoDB Connection Successful"))
     .catch(err => console.error("Error connecting to MongoDB:", err));
 
